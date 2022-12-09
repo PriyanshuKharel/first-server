ed500380dea186ba8b912b7a9146ef561fcff14c
Priyanshu Kharel <kharelpriyanshu01@gmail.com>
2022-12-09 16:34:56 +0545

    server starting

---
+++ server.js
@@ -0,0 +1,12 @@
+const http = require("http");
+const port = 8081;
+http.createServer((req,res) => {
+    res.writeHead(200,{"Content Type":"text/html"});
+    res.write("<h4>Hello, priyanshu this is my first server</h4>")
+    res.end();
+
+}).listen(port,() =>{
+    console.log(`My Node js server started on port ${port}`);
+
+});
+//http://localhost:8081
